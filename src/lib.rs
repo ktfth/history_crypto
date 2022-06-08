ee005a999500d477fff73f8599d35a737c16e282
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-06-07 21:52:24 -0300

    feat: add xor cipher and decipher

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use hex::ToHex;
+use hex;
 
 pub fn char_code(value: &str) -> u16 {
     let v: Vec<u16> = value.encode_utf16().collect();
@@ -15,9 +15,9 @@ pub fn cipher_xor(message: &str, key: &str) -> String {
         let k = key_sequence[key_charger].to_string();
         let d_char_code = char_code(&d);
         let k_char_code = char_code(&k);
-        let current = d_char_code ^ k_char_code;
+        let current = d_char_code as u8 ^ k_char_code as u8;
 
-        encrypted_message.push(std::char::from_digit(current.into(), 16).unwrap());
+        encrypted_message.push(current as char);
 
         key_charger += 1;
 
@@ -26,7 +26,30 @@ pub fn cipher_xor(message: &str, key: &str) -> String {
         }
     }
     let result = encrypted_message.iter().cloned().collect::<String>();
-    result.as_str().encode_hex::<String>()
+    hex::encode(result)
+}
+
+pub fn decipher_xor(hash: &str, key: &str) -> String {
+    let mut key_charger = 0;
+    let mut decrypted_message: Vec<_> = Vec::new();
+    let hash_bytes = hex::decode(hash).unwrap();
+
+    for d in hash_bytes {
+        let k_data = [key.as_bytes()[key_charger]];
+        let k = std::str::from_utf8(&k_data).unwrap();
+        let k_char_code = char_code(&k);
+        let current = d ^ k_char_code as u8;
+
+        decrypted_message.push(current as char);
+
+        key_charger += 1;
+
+        if key.len() == key_charger {
+            key_charger = 0;
+        }
+    }
+    let result = decrypted_message.iter().cloned().collect::<String>();
+    result
 }
 
 #[cfg(test)]
@@ -36,6 +59,11 @@ mod tests {
     #[test]
     fn ciphering_xor() {
         assert_eq!(97, char_code("a"));
-        assert_eq!("6539646239".to_string(), cipher_xor("hello", "flag"));
+        assert_eq!("0e090d0b09".to_string(), cipher_xor("hello", "flag"));
+    }
+
+    #[test]
+    fn deciphering_xor() {
+        assert_eq!("hello", decipher_xor("0e090d0b09", "flag"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod lib;
+
+use lib::{cipher_xor, decipher_xor};
+
 fn main() {
-    println!("{:?}", 5 ^ 1);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 4 && args[1] == "cipher" {
+        println!("{}", cipher_xor(&args[2], &args[3]));
+    } else if args.len() == 4 && args[1] == "decipher" {
+        println!("{}", decipher_xor(&args[2], &args[3]));
+    } else {
+        println!("Options available \"cipher\" and \"decipher\".");
+    }
 }
